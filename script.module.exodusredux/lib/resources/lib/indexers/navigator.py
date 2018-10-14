20f51f6e31174a23b7e31e70a90ff3c22232ba57
SerpentDrago <serpentdrago@gmail.com>
2018-10-14 10:56:21 -0400

    Kodi 18 Search fix
    per instructions here > https://old.reddit.com/r/Addons4Kodi/comments/9kgpau/alternative_xbox_one_kodi_18_search_fix/?context=10

--- script.module.exodusredux/lib/resources/lib/indexers/navigator.py
+++ script.module.exodusredux/lib/resources/lib/indexers/navigator.py
@@ -328,14 +328,13 @@ class navigator:
         cache.cache_clear_search()
         control.infoDialog(control.lang(32057).encode('utf-8'), sound=True, icon='INFO')
 
-    def clearCacheAll(self):
+    def clearCacheSearch(self):
         control.idle()
-        yes = control.yesnoDialog(control.lang(32056).encode('utf-8'), '', '')
-        if not yes: return
-        from resources.lib.modules import cache
-        cache.cache_clear_all()
-        control.infoDialog(control.lang(32057).encode('utf-8'), sound=True, icon='INFO')
-
+        if control.yesnoDialog(control.lang(32056).encode('utf-8'), '', ''):
+            control.setSetting('tvsearch', '')
+            control.setSetting('moviesearch', '')
+            control.refresh()
+            
     def addDirectoryItem(self, name, query, thumb, icon, context=None, queue=False, isAction=True, isFolder=True):
         try: name = control.lang(name).encode('utf-8')
         except: pass
